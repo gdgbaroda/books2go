e11577f25b861679f46e9ab0ae518d102b44a3b7
Chintan <chints1193@gmail.com>
2018-08-24 22:30:34 +0530

    Changed login flow.

--- app/lib/login.dart
+++ app/lib/login.dart
@@ -33,7 +33,7 @@ class LoginWidgetState extends State<LoginWidget> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text('Books2Go'),
       ),
       body: Center(
         child: Column(
@@ -42,7 +42,7 @@ class LoginWidgetState extends State<LoginWidget> {
           children: this.createBody(rootContext.user),
         ),
       ),
-      drawer: NavDrawer(),
+      drawer: rootContext.user == null ? null : NavDrawer(),
     );
   }
 
@@ -53,15 +53,69 @@ class LoginWidgetState extends State<LoginWidget> {
     if (this.loading) {
       children.add(new CircularProgressIndicator());
     } else if (user == null) {
-      children.add(this.createSignInButton());
+      children.addAll(this.createLoginScreen());
     } else {
-      children.add(Text("Welcome ${user.displayName} !"));
+      children.addAll(this.createWelcomeScreen(user));
     }
 
     return children;
   }
 
-  Widget createSignInButton() {
-    return RaisedButton(child: Text('SIGN IN'), onPressed: _login);
+  List<Widget> createLoginScreen() {
+    return <Widget>[
+      Icon(
+        Icons.book,
+        size: 80.0,
+        color: Colors.amber,
+      ),
+      Container(
+        margin: const EdgeInsets.only(top: 10.0),
+        child: Text('Books2Go',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0,
+                color: Colors.amber)),
+      ),
+      Container(
+        margin: const EdgeInsets.only(top: 6.0),
+        child: Text('Your friendly book manager',
+            style: TextStyle(fontSize: 15.0, color: Colors.black87)),
+      ),
+      Container(
+        margin: const EdgeInsets.only(top: 32.0),
+        child: RaisedButton(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(right: 4.0),
+                child: Icon(Icons.account_circle),
+              ),
+              Text('Sign-In with Google'),
+            ],
+          ),
+          color: Colors.white,
+          textColor: Colors.black54,
+          splashColor: Color.fromARGB(255, 222, 222, 222),
+          onPressed: _login,
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> createWelcomeScreen(FirebaseUser user) {
+    return <Widget>[
+      Icon(
+        Icons.tag_faces,
+        size: 100.0,
+        color: Colors.amber,
+      ),
+      Container(
+        margin: const EdgeInsets.only(top: 10.0),
+        child: Text("Welcome ${user.displayName}!",
+            style: TextStyle(fontSize: 15.0, color: Colors.black87)),
+      ),
+    ];
   }
 }
